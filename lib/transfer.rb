41ee0a0fc74b58d61820c39278382608557aa7ca
Saida Davis <saihdavis@gmail.com>
2020-08-10 16:30:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,8 @@
 require "pry"
 class Transfer
   
-  attr_accessor :sender, :receiver, :amount
-  attr_reader :status
+  attr_accessor :sender, :receiver, :amount, :status
+  
   
   def initialize(sender, receiver, amount)
     @sender = sender
