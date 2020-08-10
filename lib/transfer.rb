fbfdcf1bb7cea38da4e7811afe16f74d8a9ee624
Saida Davis <saihdavis@gmail.com>
2020-08-10 16:28:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,8 +34,8 @@ end
 
   def reverse_transfer 
     if @status = "complete" 
-      @receiver.balance -= @amount 
-      @sender.balance += @amount 
+      
+      @sender.balance += @amount @receiver.balance -= @amount 
       @receiver.balance
       @status = "reversed"
     else
